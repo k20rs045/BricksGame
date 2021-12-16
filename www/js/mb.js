0d6ec0ba2cd53b777f688d1dd97c4b9a99461926
k20rs045 <k20rs045@st.kyusan-u.ac.jp>
2021-12-16 10:35:10 +0900

    SaveData function fixed

--- www/js/mb.js
+++ www/js/mb.js
@@ -1,8 +1,16 @@
-// This is a JavaScript file
-var ncmb = new NCMB("5853cb26f8a155c90bb80a3e341f021f497ef77496311ab6ba01f38c561e50d3","d1b81480fc74e86cc1bb2d28123e1107c23c9055afd935b396b264df60e42dba");
 
-var save_mb = function(score){
-  var name = prompt("Please write your name: ");
-  var Score = new ncmb.DataStore("BricksGame");
-  new Score().set("name",name).set("score",score).save();
+var ncmb = new NCMB("5853cb26f8a155c90bb80a3e341f021f497ef77496311ab6ba01f38c561e50d3", "d1b81480fc74e86cc1bb2d28123e1107c23c9055afd935b396b264df60e42dba");
+
+function save_mb(score){
+  let name = prompt("Please write your name: ","");
+  let saveScore = ncmb.DataStore("BricksGame");
+  let saveData = new saveScore();
+  saveData.set("name",name).set("score",score)
+    .save()
+    .then (function(m){
+      console.log("保存に成功しました。");
+    })
+    .catch(function(error){
+      console.log("保存に失敗しました。");
+    })   
 }
